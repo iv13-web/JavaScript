a99ec10c4f36ed948b4dbf5cfd266e6a5e7ff543
iv13-web <iivanovspg@gmail.com>
2021-03-07 21:56:47 +0300

    hw2 expanded

--- hw-2 (table 100x100)/main.js
+++ hw-2 (table 100x100)/main.js
@@ -20,17 +20,46 @@ table.innerHTML = b;
 
 console.log(table.innerHTML)
 
+//---------------------------------------------------------------------------------------------------------------
 
+let table2 = document.getElementById('table2');
+let magnifier = 0;
 
+let allRows = '';
+for (i = 0; i < 17; i++) {
 
+  let oneRow = ''
+  for (j = magnifier; j < magnifier + 23; j++) {
+    oneRow += '<td>' + (j + 1) + '</td>';
+  } 
+  magnifier += 23;
+  allRows += '<tr>' + oneRow + '</tr>';
+}
 
+console.log(allRows)
+table2.innerHTML = allRows
 
+//---------------------------------------------------------------------------------------------------------------
 
+let table3 = document.getElementById('table3');
 
+let fullTable = '';
+let increaser = 0;
 
+for (i = 0; i < 7; i++){
+  
+  let singleRow = '';
+  for (j = increaser; j < increaser + 7; j++) {
+    singleRow += '<td>' + (j + 1) + '</td>';
+  }
+  
+  increaser += 7;
+  fullTable += '<tr>' + singleRow + '</tr>';
+  // console.log(fullTable);
+}  
 
-
-
+// console.log(fullTable);
+table3.innerHTML = fullTable;
 
 
 
